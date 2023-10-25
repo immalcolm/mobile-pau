443f689b0d497cfc1cce9d1ddb70d84fcfaa6762
immalcolm <malcolm_yam@np.edu.sg>
2023-10-25 18:33:04 +0800

    clean for activity 1

--- module1/hello_world.java
+++ module1/hello_world.java
@@ -1,7 +1,8 @@
+//package here references the folder name
 package module1;
 
 public class hello_world {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
